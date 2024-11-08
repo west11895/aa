fc7fa30fb5a490e0219d2f7211ba0a3808126d45
大西弘晃 <hiro002405@icloud.com>
2024-11-08 17:36:38 +0900

    Initial Commit

---
+++ aa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  aa
+//
+//  Created by 大西弘晃 on 2024/11/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ aaTests/aaTests.swift
@@ -0,0 +1,17 @@
+//
+//  aaTests.swift
+//  aaTests
+//
+//  Created by 大西弘晃 on 2024/11/08.
+//
+
+import Testing
+@testable import aa
+
+struct aaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
